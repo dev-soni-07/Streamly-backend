a880823ef71e4a6c516acd8d06fa80a429b519ae
Dev Soni <devsoni071103@gmail.com>
2024-07-24 22:02:19 +0530

    User Registration Controller and minor changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (err, req, res, next) {
     // Middleware to hash the password before saving
     if (!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
