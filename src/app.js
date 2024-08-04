095fee5b452de95c0a534d4e6c96e5677ca5eb87
Dev Soni <devsoni071103@gmail.com>
2024-08-04 14:08:00 +0530

    Added all other required files for routes and controllers

--- src/app.js
+++ src/app.js
@@ -32,13 +32,30 @@ app.use(cookieParser())
 
 // Routes import
 import userRouter from "./routes/user.routes.js";
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 // Routes declaration
 app.use("/api/v1/users", userRouter);
 // http://localhost:8000/api/v1/users/<route>
 
-app.get("/", (req, res) => {
-    res.send("<h1>Streamly: HOME PAGE</h1>");
-});
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+
+// app.get("/", (req, res) => {
+//     res.send("<h1>Streamly: HOME PAGE</h1>");
+// });
 
 export { app };
